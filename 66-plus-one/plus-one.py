7f6442bb8d1a72b6b25633511fdb67a46ef26603
Likithpavan <likithpavan1234@gmail.com>
2025-07-13 23:10:20 +0530

    Time: 2 ms (4.69%) | Memory: 17.7 MB (79.40%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,15 +1,5 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-      r=0
-      rs=[]
-      digits.reverse()
-      for i,dig in enumerate(digits):
-            r+=dig*(10**i)
-      r+=1
-      st=str(r)
-      for dig in st:
-           n=int(dig)
-           rs.append(n)
-      return rs
-
-        
+     num = int(''.join(map(str, digits))) + 1
+     rs = [int(d) for d in str(num)]
+     return rs
