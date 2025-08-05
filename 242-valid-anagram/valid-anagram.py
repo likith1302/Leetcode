2de31c22fd0c730871542fe2f82f99662cbcf0eb
Likithpavan <likithpavan1234@gmail.com>
2025-08-05 14:45:35 +0530

    Time: 15 ms (42.22%) | Memory: 18.6 MB (5.07%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,16 +2,8 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
      if len(s) != len(t):
         return False
+     return sorted(s)==sorted(t)
 
-     freq = {}
-
-     for ch in s:
-        freq[ch] = freq.get(ch, 0) + 1
-
-     for ch in t:
-        if ch not in freq or freq[ch] == 0:
-            return False
-        freq[ch] -= 1
-
-     return True
+     
+   
         
