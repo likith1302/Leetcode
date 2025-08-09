4fd2bd3d6bd3f6da1e6b739704bb3284a07bbdb1
Likithpavan <likithpavan1234@gmail.com>
2025-08-09 21:37:33 +0530

    Time: 451 ms (88.54%) | Memory: 20.7 MB (69.55%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -5,6 +5,8 @@ class Solution:
         for i in range(len(nums) - 2):
             if i > 0 and nums[i]== nums[i - 1]:
                 continue
+            if nums[i]>0:
+                break
             l, r = i + 1, len(nums) - 1
             while l < r:
                 traget = nums[i] + nums[l] + nums[r]
