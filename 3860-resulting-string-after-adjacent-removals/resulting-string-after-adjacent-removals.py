64b2488d1a445cf18afd6020b4b07126eb97200e
Likithpavan <likithpavan1234@gmail.com>
2025-08-11 10:50:04 +0530

    Time: 472 ms (77.16%) | Memory: 19.8 MB (40.52%) - LeetSync

--- 3860-resulting-string-after-adjacent-removals/resulting-string-after-adjacent-removals.py
+++ 3860-resulting-string-after-adjacent-removals/resulting-string-after-adjacent-removals.py
@@ -1,12 +1,12 @@
 class Solution:
-    def resultingString(self, s: str) -> str:
-        i=0
-        while i<len(s)-1:
-            r=ord(s[i])-ord(s[i+1])
-            if abs(r)== 1 or abs(r)==25:
-                s=s[:i]+s[i+2:]
-                i=max(i-1,0)
-            else:
-                i+=1
-        return s
-            
+  def resultingString(self, s: str) -> str:
+    stack = []
+    for ch in s:
+        if stack:
+            r = ord(stack[-1]) - ord(ch)
+            if abs(r) == 1 or abs(r) == 25:
+                stack.pop()
+                continue
+        stack.append(ch)
+    return "".join(stack)
+
