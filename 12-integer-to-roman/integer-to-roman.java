ef404589f2b82a0bd90dc65b969bb21484305377
Likithpavan <likithpavan1234@gmail.com>
2026-01-27 13:41:29 +0530

    Time: 7 ms (14.47%) | Memory: 47.2 MB (6.80%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,35 +1,29 @@
 class Solution {
     public String intToRoman(int num) {
         Map<String, Integer> map = new LinkedHashMap<>();
-        String r="";
-       map.put("M", 1000);
-map.put("CM", 900);
-map.put("D", 500);
-map.put("CD", 400);
-map.put("C", 100);
-map.put("XC", 90);
-map.put("L", 50);
-map.put("XL", 40);
-map.put("X", 10);
-map.put("IX", 9);
-map.put("V", 5);
-map.put("IV", 4);
-map.put("I", 1);
+        map.put("M", 1000);
+        map.put("CM", 900);
+        map.put("D", 500);
+        map.put("CD", 400);
+        map.put("C", 100);
+        map.put("XC", 90);
+        map.put("L", 50);
+        map.put("XL", 40);
+        map.put("X", 10);
+        map.put("IX", 9);
+        map.put("V", 5);
+        map.put("IV", 4);
+        map.put("I", 1);
+
+        StringBuilder r = new StringBuilder();
 
         for (Map.Entry<String, Integer> v : map.entrySet()) {
-            int c=num/v.getValue();
-            if(c==0){
-                continue;
-            }
-            else{
-                while(c!=0){
-                    r+=v.getKey();
-                    num-=v.getValue();
-                    c--;
-                }
+            int c = num / v.getValue();
+            if (c > 0) {
+                r.append(v.getKey().repeat(c)); 
+                num -= v.getValue() * c;
             }
         }
-
-        return r;
+        return r.toString();
     }
 }
