8c266bb4ffa06309e33a11183f91ab18c25dad8f
Likithpavan <likithpavan1234@gmail.com>
2026-02-02 21:30:08 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (62.80%) - LeetSync

---
+++ 1115-valid-boomerang/valid-boomerang.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isBoomerang(int[][] points) {
+        int[] p1=points[0];
+        int[] p2=points[1];
+        int[] p3=points[2];
+        return (p2[1]-p1[1])*(p3[0]-p2[0])!=(p2[0]-p1[0])*(p3[1]-p2[1]);
+        
+    }
+}
