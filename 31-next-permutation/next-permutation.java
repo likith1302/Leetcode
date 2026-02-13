3d2d06ffef4bccc09a4c778999357c62d999025a
Likithpavan <likithpavan1234@gmail.com>
2026-02-13 21:02:42 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (67.89%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -2,26 +2,26 @@ class Solution {
     public void nextPermutation(int[] nums) {
         int i=nums.length-2;
         while(i>=0 && nums[i]>=nums[i+1]){
-            i--;
+            i--;// to find the dip in array
         }
         if(i>=0){
             int j=nums.length-1;
             while(nums[j]<=nums[i]){
-                j--;
+                j--;//to find the elemnet to replace it 
             }
-            swap(nums,i,j);
+            swap(nums,i,j);//replace 
         }
-        reverse(nums,i+1,nums.length-1);
+        reverse(nums,i+1,nums.length-1);// to maintain the next permutation 
   
         }
        private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
+        int temp = nums[i];//normal function
         nums[i] = nums[j];
         nums[j] = temp;
     }
     
     private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
+        while (start < end) {// normal function 
             swap(nums, start, end);
             start++;
             end--;
