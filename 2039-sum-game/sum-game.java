d79c679f8fd3ca05a420af9ec305816b2120a4fe
Likithpavan <likithpavan1234@gmail.com>
2026-08-23 19:56:38 +0530

    Time: 8 ms (53.28%) | Memory: 46.8 MB (83.94%) - LeetSync

---
+++ 2039-sum-game/sum-game.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int sumLeft = 0, sumRight = 0;
+        int qLeft = 0, qRight = 0;
+
+        for (int i = 0; i < n; i++) {
+            char c = num.charAt(i);
+            if (i < n / 2) {
+                if (c == '?') qLeft++;
+                else sumLeft += c - '0';
+            } else {
+                if (c == '?') qRight++;
+                else sumRight += c - '0';
+            }
+        }
+
+        // If odd number of '?', Alice wins
+        if ((qLeft + qRight) % 2 == 1) return true;
+
+        // Check balance condition
+        int diff = sumLeft - sumRight;
+        int qDiff = qRight - qLeft;
+        return diff != qDiff * 9 / 2;
+    }
+}
